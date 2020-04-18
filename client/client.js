c572eed25e45eea0a4361a9c01f6d2ccb985f3de
Ebert Toribio <eberttc@gmail.com>
2020-04-18 18:23:24 -0500

    Complete Grpc types

--- client/client.js
+++ client/client.js
@@ -7,6 +7,133 @@ var service = require('../server/protos/greet_grpc_pb')
 var grpc = require('grpc')
 
 
+function callGreetManyTime() {
+
+    var client = new service.GreetServiceClient(
+        'localhost:50051',
+        grpc.credentials.createInsecure()
+    )
+
+    //create request
+    var request = new greets.GreetManyTimesRequest()
+
+    var greeting = new greets.Greeting()
+    greeting.setFirstName('Ebert')
+    greeting.setLastName('Toribio')
+
+    request.setGreeting(greeting)
+
+    var call = client.greetManyTimes(request, () => { })
+
+    call.on('data', (response) => {
+        console.log('Client Streaming Response:', response.getResult());
+    })
+
+    call.on('status', (status) => {
+        console.log(status)
+    })
+
+    call.on('error', (error) => {
+        console.error(error)
+    })
+
+    call.on('end', () => {
+        console.log('Streaming End')
+    })
+
+}
+
+function callLongGreeting() {
+
+    var client = new service.GreetServiceClient(
+        "localhost:50051",
+        grpc.credentials.createInsecure()
+    );
+
+    var request = new greets.LongGreetRequest()
+
+    var call = client.longGreet(request, (error, response) => {
+        if (!error) {
+            console.log('Server response:' + response.getResult())
+        } else {
+            console.error(error)
+        }
+
+    })
+
+    let count = 0, intervalID = setInterval(function () {
+
+        console.log('Sending message:' + count)
+
+        var request = new greets.LongGreetRequest()
+
+        var greeting = new greets.Greeting()
+        greeting.setFirstName('Ebert')
+        greeting.setLastName('Toribio')
+
+        request.setGreet(greeting)
+
+        call.write(request)
+
+        if (++count > 3) {
+            clearInterval(intervalID)
+            call.end()//
+        }
+
+    }, 1000)
+
+
+}
+
+async function sleep(interval) {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve(), interval)
+
+    })
+}
+
+async function callBidirect(interval) {
+    var client = new service.GreetServiceClient(
+        "localhost:50051",
+        grpc.credentials.createInsecure()
+    );
+
+    var call = client.greetEveryone(request, (error, response) => {
+        console.log('Server Response:' + response)
+    })
+
+    call.on('data', (response) => {
+        console.log('Hello Client!:', response.getResult());
+    })
+
+    call.on('error', (error) => {
+        console.error(error)
+    })
+
+    call.on('end', () => {
+        console.log('Client End')
+    })
+
+    for (var i = 0; i < 10; i++) {
+
+        var greeting = new greets.Greeting()
+        greeting.setFirstName('Juan')
+        greeting.setLastName('Toribio')
+
+        var request = new greets.GreetEveryoneRequest()
+
+        request.setGreet(greeting)
+
+        call.write(request)
+
+        await sleep(1500)
+
+    }
+
+call.end()
+
+}
+
 
 function main() {
     console.log('Hello from client')
@@ -33,21 +160,21 @@ function main() {
 
     request.setGreeting(greeting)
 
-    client.greet(request, (error, response) =>{
-        if(!error){
-            console.log("Greeting response:",response.getResult());
+    client.greet(request, (error, response) => {
+        if (!error) {
+            console.log("Greeting response:", response.getResult());
         }
-        else{
+        else {
             console.log(error);
         }
     })
 
 
-
-
-
-
 }
 
 
-main()
+//main()
+//callGreetManyTime()
+//callLongGreeting()
+
+callBidirect()
--- server/index.js
+++ server/index.js
@@ -16,11 +16,107 @@ function greet(call, callback) {
     callback(null, greeting)
 }
 
+function greetManyTimes(call, callback) {
+    var firstName = call.request.getGreeting().getFirstName()
+
+    let count = 0, intervalID = setInterval(function () {
+
+        var greetManyTimesResponse = new greets.GreetManyTimesResponse()
+
+        greetManyTimesResponse.setResult(firstName)
+
+        //setup streaming
+
+        call.write(greetManyTimesResponse)
+
+        if (++count > 9) {
+            clearInterval(intervalID)
+            call.end() // we have sent all     
+
+        }
+
+    }, 1000)
+
+}
+
+function longGreet(call, callback) {
+
+    call.on('data', request => {
+
+        var fullName = request.getGreet().getFirstName() + ' '
+            + request.getGreet().getLastName();
+
+        console.log('Hello ' + fullName)
+    })
+
+    call.on('error', (error) => {
+        console.error(error)
+    })
+
+    call.on('end', () => {
+
+        var resspone = new greets.LongGreetResponse()
+        resspone.setResult('Long Greet Client Streaming')
+        callback(null, resspone)
+
+    })
+
+}
+
+async function sleep(interval) {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve(), interval)
+
+    })
+}
+async function greetEveryone(call, callback) {
+
+    call.on('data', response => {
+
+        var fullName = response.getGreet().getFirstName() + ' ' +
+            response.getGreet().getLastName()
+
+        console.log('Hello ' + fullName)
+
+    })
+
+
+    call.on('error', (error) => {
+        console.error(error)
+    })
+
+    call.on('end', () => {
+        console.log('The end BIdi')
+    })
+
+    for (var i = 0; i < 10; i++) {
+        //var greeting = new greets.Greeting()
+        //greeting.setFirstName('Ebert')
+        //greeting.setLasttName('Toribio')
+
+        var request = new greets.GreetEveryoneResponse()
+        request.setResult('Ebert Toribio')
+
+        call.write(request)
+        await sleep(1000)
+
+    }
+
+    call.end()
+
+
+}
+
 function main() {
 
     var server = new grpc.Server()
 
-    server.addService(service.GreetServiceService, { greet: greet })
+    server.addService(service.GreetServiceService, {
+        greet: greet,
+        greetManyTimes: greetManyTimes,
+        longGreet: longGreet,
+        greetEveryone: greetEveryone
+    })
 
     server.bind("127.0.0.1:50051", grpc.ServerCredentials.createInsecure())
 
